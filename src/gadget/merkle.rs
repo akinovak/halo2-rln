c176f2f47a02f74a308ce35f2a8a39b6294b3c75
Andrija <akinovak@gmail.com>
2021-12-15 18:45:07 +0100

    hash mocked merkle test

--- src/gadget/merkle.rs
+++ src/gadget/merkle.rs
@@ -67,3 +67,121 @@ impl
         Ok(node)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use halo2::{
+        dev::MockProver,
+        pasta::Fp,
+        circuit::{Layouter, SimpleFloorPlanner},
+        plonk::{Advice, Instance, Column, ConstraintSystem, Error},
+        plonk,
+    };
+    use std::marker::PhantomData;
+    use pasta_curves::pallas;
+
+    // use crate::gadget::swap::{SwapChip, SwapConfig, SwapInstruction};
+    use crate::utils::{UtilitiesInstructions, NumericCell, Numeric};
+    use super::{MerkleChip, MerkleConfig, InclusionProof};
+
+    #[derive(Clone, Debug)]
+    pub struct Config {
+        advice: [Column<Advice>; 3],
+        instance: Column<Instance>,
+        merkle_config: MerkleConfig
+    }
+
+
+    #[derive(Debug, Default)]
+    pub struct Circuit {
+        leaf: Option<Fp>,
+        siblings: [Option<Fp>; 3],
+        root: Option<Fp>,
+        leaf_pos: [Option<bool>; 3]
+    }
+
+    impl UtilitiesInstructions<pallas::Base> for Circuit {
+        type Var = NumericCell<pallas::Base>;
+    }
+
+    impl plonk::Circuit<pallas::Base> for Circuit {
+        type Config = Config;
+        type FloorPlanner = SimpleFloorPlanner;
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<pallas::Base>) -> Self::Config {
+
+            let advice = [
+                meta.advice_column(),
+                meta.advice_column(),
+                meta.advice_column(),
+            ];
+
+            let instance = meta.instance_column();
+            meta.enable_equality(instance.into());
+
+            for advice in advice.iter() {
+                meta.enable_equality((*advice).into());
+            }
+
+            let merkle_config = MerkleChip::configure(meta, advice);
+
+            Config {
+                advice, 
+                instance,
+                merkle_config
+            }
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<pallas::Base>,
+        ) -> Result<(), Error> {
+            let config = config.clone();
+
+            let leaf = self.load_private(
+                layouter.namespace(|| "witness leaf"),
+                config.advice[0],
+                self.leaf,
+            )?;
+
+            let chip = MerkleChip::construct(config.merkle_config.clone());
+            let inclusion_proof = InclusionProof {
+                merkle_chip: chip,
+                siblings: self.siblings,
+                leaf_pos: self.leaf_pos,
+                _marker: PhantomData::<pallas::Base>
+            };
+
+            let root = 
+                inclusion_proof.calculate_root(
+                    layouter.namespace(|| "merkle root"),
+                    leaf
+                )?;
+
+            println!("{:?}", root.value());
+
+            Ok({})
+        }
+    }
+
+    #[test]
+    fn inclusion_proof_test() {
+        let k = 4;
+    
+        let circuit = Circuit {
+            leaf: Some(Fp::from(100)),
+            siblings: [Some(Fp::from(1)); 3],
+            root: Some(Fp::from(2)), 
+            leaf_pos: [Some(false); 3]
+        };
+
+        let public_inputs = vec![];
+        let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
+    }
+}
